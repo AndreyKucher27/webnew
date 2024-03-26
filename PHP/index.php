f1f7636e63a924ed63f0062bddd6e1986e9f9e50
AndreyKucher27 <144955355+AndreyKucher27@users.noreply.github.com>
2024-03-26 21:40:22 +0300

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -102,9 +102,9 @@ if ($errors) {
     exit();
 }
 
-$user = 'u67451';
-$pass = '5546450';
-$db = new PDO('mysql:host=localhost;dbname=u67451', $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+$user = 'u67444';
+$pass = '4484182';
+$db = new PDO('mysql:host=localhost;dbname=u67444', $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 foreach ($_POST['languages'] as $language) {
